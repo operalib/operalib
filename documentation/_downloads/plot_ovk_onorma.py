8a4b6112baae4ffd77df5c35f0d3e20721753245
operalib <ro.brault@gmail.com>
2016-07-06 19:22:04 +0000

    Pushing the docs for revision for branch: master, commit b49d982b725e8c1aa2e0c52e08194edea3b4ff65

---
+++ documentation/_downloads/plot_ovk_onorma.py
@@ -0,0 +1,87 @@
+"""
+======================================================
+Online Learning with Operator-Valued kernels
+======================================================
+
+An example to illustrate online learning with operator-valued
+kernels.
+"""
+
+import operalib as ovk
+import numpy as np
+import matplotlib.pyplot as plt
+
+import time
+
+np.random.seed(0)
+
+n = 5000
+d = 20
+p = 4
+X = np.random.rand(n, d)
+
+
+def phi(X):
+    """Generate data according to Evgeniou, C. A. Micchelli, and M. Pontil.
+
+    'Learning multiple tasks with kernel methods.' 2005.
+    """
+    return np.hstack((X[:, 0:1] ** 2,
+                      X[:, 3:4] ** 2,
+                      X[:, 0:1] * X[:, 1:2],
+                      X[:, 2:3] * X[:, 4:5],
+                      X[:, 1:2],
+                      X[:, 3:4],
+                      np.ones((n, 1))))
+
+
+print('Generating Data')
+y = np.dot(phi(X), np.random.multivariate_normal(np.zeros(7),
+                                                 np.diag([0.5, 0.25, 0.1, 0.05,
+                                                          0.15, 0.1, 0.15]),
+                                                 p).T)
+
+# Link components to a common mean.
+y = .5 * y + 0.5 * np.mean(y, axis=1).reshape(-1, 1)
+
+est = ovk.ONORMA('DGauss', A=.8 * np.eye(p) + .2 * np.ones((p, p)), gamma=.25,
+                 learning_rate=ovk.InvScaling(1., .5), lbda=0.00001)
+
+print('Fitting Joint...')
+start = time.time()
+est.partial_fit(X[0, :].reshape(1, -1), y[0, :].reshape(1, -1), n, p)
+err = np.empty(n)
+err[0] = np.linalg.norm(y[0, :]) ** 2
+for t in range(1, n):
+    err[t] = np.linalg.norm(est.predict(X[t, :].reshape(1, -1)) -
+                            y[t, :].reshape(1, -1)) ** 2
+    est.partial_fit(X[t, :].reshape(1, -1), y[t, :].reshape(1, -1))
+print('Joint training time:', time.time() - start)
+print('Joint MSE:', err[-1])
+
+err_c = np.cumsum(err) / (np.arange(n) + 1)
+plt.semilogy(np.linspace(0, 100, err_c.size), err_c, label='Joint')
+
+est = ovk.ONORMA('DGauss', A=1. * np.eye(p) + .0 * np.ones((p, p)), gamma=.25,
+                 learning_rate=ovk.InvScaling(1., .5), lbda=0.00001)
+
+print('Fitting Independant...')
+start = time.time()
+est.partial_fit(X[0, :].reshape(1, -1), y[0, :].reshape(1, -1), n, p)
+err = np.empty(n)
+err[0] = np.linalg.norm(y[0, :]) ** 2
+for t in range(1, n):
+    err[t] = np.linalg.norm(est.predict(X[t, :].reshape(1, -1)) -
+                            y[t, :].reshape(1, -1)) ** 2
+    est.partial_fit(X[t, :].reshape(1, -1), y[t, :].reshape(1, -1))
+print('Independant training time:', time.time() - start)
+print('Independant MSE:', err[-1])
+
+err_c = np.cumsum(err) / (np.arange(n) + 1)
+plt.semilogy(np.linspace(0, 100, err_c.size), err_c, label='Independant')
+
+plt.title('Online learning with ONORMA')
+plt.xlabel('Size of the Training set (%)')
+plt.ylabel('MSE')
+plt.legend()
+plt.show()
--- documentation/_downloads/plot_ovk_quantile_regression.py
+++ documentation/_downloads/plot_ovk_quantile_regression.py
@@ -15,59 +15,18 @@ constraints.
 
 # -*- coding: utf-8 -*-
 
-from operalib import Quantile
+from operalib import Quantile, generate_quantile_data
 
 import numpy as np
 import time
-from scipy.stats import norm
 import matplotlib.pyplot as plt
-
-
-def toy_data(n=50, probs=[0.5], noise=1.):
-    """Sine wave toy dataset.
-
-    Parameters
-    ----------
-    n : {integer}
-        Number of samples to generate.
-
-    probs : {list}, shape = [n_quantiles]
-        Probabilities (quantiles levels).
-
-    Returns
-    -------
-    X : {array}, shape = [n]
-        Input data.
-
-    y : {array}, shape = [n]
-        Targets.
-
-    quantiles : {array}, shape = [n x n_quantiles]
-        True conditional quantiles.
-    """
-    t_min, t_max = 0., 1.5  # Bounds for the input data
-    t_down, t_up = 0., 1.5  # Bounds for the noise
-    t = np.random.rand(n) * (t_max - t_min) + t_min
-    t = np.sort(t)
-    pattern = -np.sin(2 * np.pi * t)  # Pattern of the signal
-    enveloppe = 1 + np.sin(2 * np.pi * t / 3)  # Enveloppe of the signal
-    pattern = pattern * enveloppe
-    # Noise decreasing std (from noise+0.2 to 0.2)
-    noise_std = 0.2 + noise * (t_up - t) / (t_up - t_down)
-    # Gaussian noise with decreasing std
-    add_noise = noise_std * np.random.randn(n)
-    observations = pattern + add_noise
-    quantiles = [pattern + norm.ppf(p, loc=np.zeros(n),
-                                    scale=np.fabs(noise_std)) for p in probs]
-    return t[:, None], observations, quantiles
-
 
 np.random.seed(0)
 
 print("Creating dataset...")
 probs = np.linspace(0.1, 0.9, 5)  # Quantile levels of interest
-x_train, y_train, z_train = toy_data(50)
-x_test, y_test, z_test = toy_data(1000, probs=probs)
+x_train, y_train, z_train = generate_quantile_data(50)
+x_test, y_test, z_test = generate_quantile_data(1000, probs=probs)
 
 print("Fitting...")
 # Joint quantile regression

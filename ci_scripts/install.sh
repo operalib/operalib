e3d5a6d8d629e3a77e558979d145bb530a90cbbb
Romain Raymond Brault <ro.brault@gmail.com>
2017-03-16 14:12:42 +0100

    changed to miniconda latest

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -43,7 +43,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
         fi
     chmod +x miniconda.sh && ./miniconda.sh -b
     cd ..
-    export PATH=/home/travis/miniconda/bin:$PATH
+    export PATH=/home/travis/miniconda3/bin:$PATH
     conda update --yes conda
     popd
 

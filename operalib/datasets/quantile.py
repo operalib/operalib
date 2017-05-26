dc10dae519d14938022dde2d96479910ec8859de
Romain Raymond Brault <ro.brault@gmail.com>
2017-05-26 14:17:31 +0200

    cleaning code

--- operalib/datasets/quantile.py
+++ operalib/datasets/quantile.py
@@ -5,7 +5,7 @@ from numpy.random import rand, randn
 from numpy import sort, sin, asarray, pi
 
 
-def toy_data_quantile(n=50, probs=[0.5], noise=1.):
+def toy_data_quantile(n_points=50, probs=None, noise=1.):
     """Sine wave toy dataset.
 
     Parameters
@@ -27,18 +27,21 @@ def toy_data_quantile(n=50, probs=[0.5], noise=1.):
     quantiles : {array}, shape = [n x n_quantiles]
         True conditional quantiles.
     """
+    if probs is None:
+        probs = [.5]
+
     t_min, t_max = 0., 1.5  # Bounds for the input data
     t_down, t_up = 0., 1.5  # Bounds for the noise
-    t = rand(n) * (t_max - t_min) + t_min
-    t = sort(t)
-    pattern = -sin(2 * pi * t)  # Pattern of the signal
-    enveloppe = 1 + sin(2 * pi * t / 3)  # Enveloppe of the signal
+    t_rand = rand(n_points) * (t_max - t_min) + t_min
+    t_rand = sort(t_rand)
+    pattern = -sin(2 * pi * t_rand)  # Pattern of the signal
+    enveloppe = 1 + sin(2 * pi * t_rand / 3)  # Enveloppe of the signal
     pattern = pattern * enveloppe
     # Noise decreasing std (from noise+0.2 to 0.2)
-    noise_std = 0.2 + noise * (t_up - t) / (t_up - t_down)
+    noise_std = 0.2 + noise * (t_up - t_rand) / (t_up - t_down)
     # Gaussian noise with decreasing std
-    add_noise = noise_std * randn(n)
+    add_noise = noise_std * randn(n_points)
     observations = pattern + add_noise
     quantiles = [pattern + asarray([norm.ppf(p, loc=0., scale=abs(noise_c))
-                 for noise_c in noise_std]) for p in probs]
-    return t[:, None], observations, quantiles
+                                    for noise_c in noise_std]) for p in probs]
+    return t_rand[:, None], observations, quantiles

1693ecad32b7877da70f2303d7154aa6a9deb300
Romain Raymond Brault <ro.brault@gmail.com>
2017-05-28 15:23:34 +0200

    quantile

--- examples/plot_ovk_quantile_regression.py
+++ examples/plot_ovk_quantile_regression.py
@@ -14,56 +14,51 @@ constraints.
 # License: MIT
 
 # -*- coding: utf-8 -*-
+import time
+
+import numpy as np
+from numpy.random import seed
+import matplotlib.pyplot as plt
 
 from operalib import Quantile, toy_data_quantile
 
-import numpy as np
-import time
-import matplotlib.pyplot as plt
 
-np.random.seed(0)
+def main():
+    """example of multiple quantile regression."""
+    seed(0)
 
-print("Creating dataset...")
-probs = np.linspace(0.1, 0.9, 5)  # Quantile levels of interest
-x_train, y_train, z_train = toy_data_quantile(50)
-x_test, y_test, z_test = toy_data_quantile(1000, probs=probs)
+    print("Creating dataset...")
+    probs = np.linspace(0.1, 0.9, 5)  # Quantile levels of interest
+    x_train, y_train, _ = toy_data_quantile(50)
+    x_test, y_test, z_test = toy_data_quantile(1000, probs=probs)
 
-print("Fitting...")
-# Joint quantile regression
-lbda = 1e-2
-gamma = 1e1
-joint = Quantile(probs=probs, kernel='DGauss', lbda=lbda, gamma=gamma,
-                 gamma_quantile=1e-2)
-# Independent quantile regression
-ind = Quantile(probs=probs, kernel='DGauss', lbda=lbda, gamma=gamma,
-               gamma_quantile=np.inf)
-# Independent quantile regression (with non-crossing constraints)
-nc = Quantile(probs=probs, kernel='DGauss', lbda=lbda, gamma=gamma,
-              gamma_quantile=np.inf, nc_const=True)
+    print("Fitting...")
+    methods = {'Joint':
+               Quantile(probs=probs, kernel='DGauss', lbda=1e-2, gamma=1e1,
+                        gamma_quantile=1e-2),
+               'Independent': Quantile(probs=probs, kernel='DGauss', lbda=1e-2,
+                                       gamma=1e1, gamma_quantile=np.inf),
+               'Non-crossing': Quantile(probs=probs, kernel='DGauss',
+                                        lbda=1e-2, gamma=1e1,
+                                        gamma_quantile=np.inf, nc_const=True)}
+    # Fit on training data
+    for name, reg in methods.items():
+        start = time.time()
+        reg.fit(x_train, y_train)
+        print(name + ' leaning time: ', time.time() - start)
+        print(name + ' score ', reg.score(x_test, y_test))
 
-# Fit on training data
-methods = {'joint': joint,
-           'independant': ind,
-           'non-crossing': nc}
-for name, reg in methods.items():
-    start = time.time()
-    reg.fit(x_train, y_train)
-#    pred = joint.predict(x_test)
-    print(name + ' leaning time: ', time.time() - start)
-    print(name + ' score ', reg.score(x_test, y_test))
+    # Plot the estimated conditional quantiles
+    plt.figure(figsize=(12, 7))
+    for i, method in enumerate(['Joint', 'Independent', 'Non-crossing']):
+        plt.subplot(1, 3, i + 1)
+        plt.plot(x_train, y_train, '.')
+        for quantile in methods[method].predict(x_test):
+            plt.plot(x_test, quantile, '-')
+        for quantile in z_test:
+            plt.plot(x_test, quantile, '--')
+        plt.title(method)
+    plt.show()
 
-# Plot the estimated conditional quantiles
-
-plt.figure(figsize=(12, 7))
-for (i, (reg, title)) in enumerate(
-    [(joint, 'Joint'),
-     (ind, 'Independent'),
-     (nc, 'Independent (non-crossing)')]):
-    plt.subplot(1, 3, i + 1)
-    plt.plot(x_train, y_train, '.')
-    for q in reg.predict(x_test):
-        plt.plot(x_test, q, '-')
-    for q in z_test:
-        plt.plot(x_test, q, '--')
-    plt.title(title)
-plt.show()
+if __name__ == '__main__':
+    main()
